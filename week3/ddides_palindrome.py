4c961e2f00fdb9738442cf241ca8eabe88fc79e4
Daniel Dides <senorequeso@gmail.com>
2015-11-02 21:15:43 -0500

    ddides_palindrome.py - Added in recursive solution

--- week3/ddides_palindrome.py
+++ week3/ddides_palindrome.py
@@ -21,17 +21,34 @@ def palindrome_itertive(a):
                 end -= 1
         return True
 
+def palindrome_recursive(a):
+    """ OH GOD HERE WE GO AGAIN AND AGAIN AND AGAIN AND AGAIN """
+    # Base case
+    if len(a) <= 1:
+        return True
+    else:
+        if a[0] == a[len(a)-1]:
+            return palindrome_recursive(a[1:-1])
+        else:
+            return False
        
 
 def main():
     print(palindrome_golf('a'))
     print(palindrome_golf('abc'))
     print(palindrome_golf('abcba'))
+    print(palindrome_golf('racecar'))
     print('---')
     print(palindrome_itertive('a'))
     print(palindrome_itertive('abc'))
     print(palindrome_itertive('abcba'))
     print(palindrome_itertive('racecar'))
+    print('---')
+    print(palindrome_recursive('a'))
+    print(palindrome_recursive('abc'))
+    print(palindrome_recursive('abcba'))
+    print(palindrome_recursive('racecar'))
+
 
 if __name__ == '__main__':
     main()
