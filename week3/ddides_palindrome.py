376887c2b25257c9b9a07ca280bd21ecd3757b08
Daniel Dides <senorequeso@gmail.com>
2015-11-03 16:35:13 -0500

    This works. But dear God, is it hacky.

--- week3/ddides_palindrome.py
+++ week3/ddides_palindrome.py
@@ -32,25 +32,58 @@ def palindrome_recursive(a):
             return False
 
 def longest_palindrome(a):
-    """ Returns the longest contiguous palindrome in a given string """
+    """ 
+    Returns the longest contiguous palindrome in a given string. 
+    Iteratively go through the string and pass all substrings through
+    function to check if they're palindromes. Store the longest. Then
+    increase starting character and do it again.
+
+    THIS IS BAD. If the whole string is a palindrome, I take _as long as possible_
+    to figure that out. Start big, work smaller.
+    """
+    # This is some spaghetti if I've ever seen some. In the case where the whole work is
+    # a palindrome, we can go ahead and skip. BUT ONLY IN THAT INSTANCE BECAUSE IT'S BAD
+    if palindrome_golf(a):
+        return a
+
+    # Enter the bad
+    # Set the max so far to the first character
+    max_so_far = a[0]
+    for length in range(2,len(a)-1):
+        start = 0
+        while start <= len(a) - 1:
+            sub = a[start:start+length]
+            cur = palindrome_golf(sub)
+            if cur is True and len(sub) > len(max_so_far):
+                max_so_far = sub
+            start += 1
+
+    return max_so_far
+
+
 
        
 
 def main():
-    print(palindrome_golf('a'))
-    print(palindrome_golf('abc'))
-    print(palindrome_golf('abcba'))
-    print(palindrome_golf('racecar'))
+    # print(palindrome_golf('a'))
+    # print(palindrome_golf('abc'))
+    # print(palindrome_golf('abcba'))
+    # print(palindrome_golf('racecar'))
+    # print('---')
+    # print(palindrome_itertive('a'))
+    # print(palindrome_itertive('abc'))
+    # print(palindrome_itertive('abcba'))
+    # print(palindrome_itertive('racecar'))
+    # print('---')
+    # print(palindrome_recursive('a'))
+    # print(palindrome_recursive('abc'))
+    # print(palindrome_recursive('abcba'))
+    # print(palindrome_recursive('racecar'))
     print('---')
-    print(palindrome_itertive('a'))
-    print(palindrome_itertive('abc'))
-    print(palindrome_itertive('abcba'))
-    print(palindrome_itertive('racecar'))
-    print('---')
-    print(palindrome_recursive('a'))
-    print(palindrome_recursive('abc'))
-    print(palindrome_recursive('abcba'))
-    print(palindrome_recursive('racecar'))
+    print(longest_palindrome('abc'))
+    print(longest_palindrome('racecar'))
+    # print(longest_palindrome('abcdd'))
+    # print(longest_palindrome('abcde111111111fgh'))
 
 
 if __name__ == '__main__':
