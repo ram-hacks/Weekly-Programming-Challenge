ba735c3e7711f0ca62dd89b721e19d68a998e029
Daniel Dides <senorequeso@gmail.com>
2015-11-02 20:42:22 -0500

    ddides_palindrome.py - Updated with iterative solution

--- week3/ddides_palindrome.py
+++ week3/ddides_palindrome.py
@@ -2,10 +2,36 @@
 def palindrome_golf(a):
     return a[::-1] == a
 
+def palindrome_itertive(a):
+    """ Start at the start and the end, count inwards """
+    # TODO make this less crappy
+    if len(a) <= 1:
+        return True
+    else:
+        start = 0 
+        end = len(a) - 1
+        while start != end:
+            # print(end)
+            # print('start: ', start, ' a: ', a[start])
+            # print('end: ', end, ' a: ', a[end])
+            if not a[start] == a[end]:
+                return False
+            else:
+                start += 1
+                end -= 1
+        return True
+
+       
+
 def main():
     print(palindrome_golf('a'))
     print(palindrome_golf('abc'))
     print(palindrome_golf('abcba'))
+    print('---')
+    print(palindrome_itertive('a'))
+    print(palindrome_itertive('abc'))
+    print(palindrome_itertive('abcba'))
+    print(palindrome_itertive('racecar'))
 
 if __name__ == '__main__':
     main()
