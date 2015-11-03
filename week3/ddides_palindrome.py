f520cc3b3ab930292c70eb74258b0bc26a02a353
Daniel Dides <senorequeso@gmail.com>
2015-11-03 16:46:18 -0500

    Removing useles base-case in iterative solution

--- week3/ddides_palindrome.py
+++ week3/ddides_palindrome.py
@@ -4,21 +4,18 @@ def palindrome_golf(a):
 def palindrome_itertive(a):
     """ Start at the start and the end, count inwards """
     # TODO make this less crappy
-    if len(a) <= 1:
-        return True
-    else:
-        start = 0 
-        end = len(a) - 1
-        while start != end:
-            # print(end)
-            # print('start: ', start, ' a: ', a[start])
-            # print('end: ', end, ' a: ', a[end])
-            if not a[start] == a[end]:
-                return False
-            else:
-                start += 1
-                end -= 1
-        return True
+    start = 0 
+    end = len(a) - 1
+    while start != end:
+        # print(end)
+        # print('start: ', start, ' a: ', a[start])
+        # print('end: ', end, ' a: ', a[end])
+        if not a[start] == a[end]:
+            return False
+        else:
+            start += 1
+            end -= 1
+    return True
 
 def palindrome_recursive(a):
     """ OH GOD HERE WE GO AGAIN AND AGAIN AND AGAIN AND AGAIN """
