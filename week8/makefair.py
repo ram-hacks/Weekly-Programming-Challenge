d2ec82f13ec858f3e77346d56945a3bedd537324
Christopher Glasz <cdglasz@users.noreply.github.com>
2016-03-29 16:42:20 -0400

    Code golfier

--- week8/makefair.py
+++ week8/makefair.py
@@ -1,2 +1,1 @@
-fair = { ('0','0'):'', ('0','1'):'0', ('1','0'):'1', ('1','1'):'' }
-makefair = lambda s: ''.join(fair[p] for p in zip(s[::2], s[1::2]))
+makefair = lambda s: ''.join(p[0] for p in zip(s[::2], s[1::2]) if p[0]!=p[1])
