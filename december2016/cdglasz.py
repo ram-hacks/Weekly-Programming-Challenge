f327a91d2f33be8994ea506b29f3bee5d00985fa
Christopher Glasz <cdglasz@users.noreply.github.com>
2016-12-08 01:40:57 -0500

    The solutions so far weren't nearly ugly enough
    In theory, this is better than the summation solution because computer scientists don't like it when numbers get too big.
    In practice, this is horrifying.
    Don't write code like this, ever.

---
+++ december2016/cdglasz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+f=lambda l:(lambda n:reduce(lambda a,b:a^b,l)^[n,1,n+1,0][n%4])(len(l)+1)
+
+if __name__ == '__main__':
+	from random import sample
+	assert f([7,8,10,3,6,5,1,2,9]) == 4
+	assert f([1,3]) == 2
+	n = 10000
+	for i in range(10):
+		l = sample(range(1,n+1),n-1)
+		assert(sorted(l + [f(l)]) == range(1,n+1))
